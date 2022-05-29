438c533086bdd04416ad5d506b39a80b5f230568
Shivangi-bharawaj <106004858+Shivangi-bharawaj@users.noreply.github.com>
2022-05-29 17:43:57 +0530

    Add files via upload

---
+++ file handling.py
@@ -0,0 +1,8 @@
+india is my country and all indians are my brothers and sisters ,i love my country and i am pround of its rich andvary heritage
+i shall always strive to be worthy of it.
+i shall give respect
+# file handling
+my file= my file. read(10)
+str1=my file. read(5)
+print("output of first read:")
+print(str1)
