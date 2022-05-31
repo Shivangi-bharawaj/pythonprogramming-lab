01ee89334aad331166a831df7b685db8cdaa6ac0
Shivangi-bharawaj <106004858+Shivangi-bharawaj@users.noreply.github.com>
2022-05-31 18:23:13 +0530

    Add files via upload

---
+++ math5.py
@@ -0,0 +1,12 @@
+import math
+ 
+# initializing the value
+test_int = 4
+test_neg_int = -3
+test_float = 0.00
+ 
+# checking exp() values
+# with different numbers
+print (math.exp(test_int))
+print (math.exp(test_neg_int))
+print (math.exp(test_float)
